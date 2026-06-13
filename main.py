54bdc246e1a4372b15abd10a990102d41da4fe95
ropydev <belloconcepcionr@gmail.com>
2026-06-13 03:09:09 -0400

    Activity function added.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI, Response
 import requests
 import textwrap
 import html
+import pygal
+from pygal.style import LightStyle
 
 app = FastAPI()
 
@@ -78,3 +80,48 @@ forks:{data["forks"]}
             return response.status_code
     except requests.ConnectionError:
         return "Connection Error"
+
+@app.get("/stats/commits-activity")
+async def activity(
+    username
+):
+    headers = {"Accept": "application/vnd.github+json"}
+    response = requests.get(f"https://api.github.com/users/{username}/repos")
+    repos = response.json()
+    reposNames = [repo["name"] for repo in repos]
+    days = list(range(1, 29))
+    commits = [0] * 28
+    for repo in reposNames:
+        data = requests.get(f"https://api.github.com/repos/{username}/{repo}/stats/commit_activity").json()
+        last4Weeks = data[-4:]
+        i = 0
+        for l in last4Weeks:
+            for d in l["days"]:
+                commits[i] += int(d)
+                i+=1
+    custom_style = LightStyle(
+        background='#000000',   # fondo transparente
+        plot_background='#606060',
+        foreground='#ffffff',       # texto blanco
+        foreground_strong='#ffffff',
+        foreground_subtle='#aaaaaa',
+        colors=('#FFFFFF',)         # color verde azulado (como tu paleta)
+    )
+
+    line_chart = pygal.Line(
+        style=custom_style,
+        show_legend=False,
+        show_y_guides=False,
+        show_x_guides=False,
+        dots_size=2,
+        stroke_style={'width': 2},
+        width=600,   # más ancho
+        height=150   # más bajo
+    )
+
+    line_chart.title = username+"'s Github Activity"
+    line_chart.x_labels = days
+    line_chart.add("", commits)
+
+    svg_data = line_chart.render()
+    return Response(content=svg_data, media_type="image/svg+xml")
